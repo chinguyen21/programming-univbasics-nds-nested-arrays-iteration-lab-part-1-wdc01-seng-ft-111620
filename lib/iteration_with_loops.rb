56d33d0c22a13f0965a615630a9143f30e84aa0e
Chi Nguyen <nguyen.thimai@northeastern.edu>
2020-10-22 12:49:51 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -1,8 +1,11 @@
 def find_even_values(src)
+  row_index = 0
   while row_index < src.length do
     element_index = 0 
     while element_index < src[row_index].length do
-      
-    
-
+      puts src[row_index][element_index] if src[row_index][element_index] % 2 == 0
+      element_index += 1 
+    end
+    row_index += 1 
+  end 
 end
